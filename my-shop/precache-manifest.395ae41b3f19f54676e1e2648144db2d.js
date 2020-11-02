21b85fa0d7cd5e7fbe2e9448d02c9f35b8e335b1
powergoose17 <powergoose17@gmail.com>
2020-11-02 15:49:00 -0500

    Create precache-manifest.395ae41b3f19f54676e1e2648144db2d.js

---
+++ my-shop/precache-manifest.395ae41b3f19f54676e1e2648144db2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "128b6b185444cf4bc707983fd01cd388",
+    "url": "/my-shop/index.html"
+  },
+  {
+    "revision": "673347270b65db4c248d",
+    "url": "/my-shop/static/css/main.5dde7dfe.chunk.css"
+  },
+  {
+    "revision": "49d4101f21ec69187d9a",
+    "url": "/my-shop/static/js/2.1f05739b.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/my-shop/static/js/2.1f05739b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "673347270b65db4c248d",
+    "url": "/my-shop/static/js/main.2b5baa11.chunk.js"
+  },
+  {
+    "revision": "aad40185760ee0916784",
+    "url": "/my-shop/static/js/runtime-main.2ff7fe06.js"
+  }
+]);
